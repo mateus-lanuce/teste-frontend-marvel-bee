f38f31df6c0792b98207a2655dbffe54b63162cb
mateus_lanuce <mateuslanuce@gmail.com>
2023-10-16 18:30:06 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="color-primary-red">pao</h1>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+
+
+.color-primary-red {
+  color: #EC1D24;
+}
+
+.bg-primary-red {
+  background-color: #EC1D24;
+}
+
+.bg-secondary-red {
+  background-color: #9F0013;
+}
+
+
+
+</style>
