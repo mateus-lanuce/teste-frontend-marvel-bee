499a87ad0ac652ea6d37720a5dcb5b09c86b818e
mateus_lanuce <mateuslanuce@gmail.com>
2023-10-17 09:58:40 -0300

    feat: :sparkles: add comics and characte page static
    created a card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+import { defineProps } from 'vue';
+
+defineProps({
+  title: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+});
+
+</script>
+
+<template>
+  <div class="d-flex flex-column gap-3">
+    <img src="../assets/images/image 3.svg" class="custom-img" alt="hero logo">
+
+    <div>
+      <h5>Spider Man</h5>
+      <p v-if="description">PAGES: 112</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+  .custom-card {
+    width: 12rem;
+    height: 12rem;
+  }
+
+  .custom-img {
+    width: 12rem;
+    height: 12rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    .custom-img {
+      width: 10rem;
+      height: 10rem;
+    }
+  }
+
+</style>
+
---
+++ src/views/CharactersView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Home from '@/components/icons/Home.vue';
+import CardComponent from '../components/CardComponent.vue';
+
+</script>
+
+<template>
+  <main class="container">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb my-4 p-2">
+        <li class="breadcrumb-item active" aria-current="page">
+          <div class="d-flex gap-2 align-items-center">
+            <Home width="16" height="16"/>
+            <span class="fw-normal">Home</span>
+          </div>
+        </li>
+      </ol>
+    </nav>
+
+    <div class="d-flex flex-wrap justify-content-between gap-4">
+      <CardComponent v-for="item in 10" :key="item" />
+    </div>
+
+  </main>
+</template>
